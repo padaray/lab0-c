208b6c59bfef865aa18b1abffc50c3b46cdc73df
RayChen <ray870317@gmail.com>
2024-02-29 02:01:44 +0800

    Implement q_delete_mid function
    Create two nodes, front and back, traverse from the head and the
    tail respectively. When encountering the same node or
    front->prev = back, delete the front node.

--- queue.c
+++ queue.c
@@ -112,7 +112,18 @@ int q_size(struct list_head *head)
 /* Delete the middle node in queue */
 bool q_delete_mid(struct list_head *head)
 {
-    // https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/
+    if (!head || list_empty(head))
+        return false;
+
+    struct list_head *front = head->next;
+    struct list_head *back = head->prev;
+
+    while (front != back && front->prev != back) {
+        front = front->next;
+        back = back->prev;
+    }
+
+    list_del(front);
     return true;
 }
 
