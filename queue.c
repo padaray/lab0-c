a3f913b7c2ffba94c13a0af0a59dcfaa311c1878
RayChen <ray870317@gmail.com>
2024-02-28 21:51:49 +0800

    Implement q_insert_tail function
    Allocate space for element_t, check if space is sufficient, create a copy
    of the input character, assign it to new_node->value, call list_add_tail
    function from list.h, and insert new_node at the tail of the list.

--- queue.c
+++ queue.c
@@ -49,6 +49,11 @@ bool q_insert_head(struct list_head *head, char *s)
 /* Insert an element at tail of queue */
 bool q_insert_tail(struct list_head *head, char *s)
 {
+    element_t *new_node = malloc(sizeof(element_t));
+    if (!new_node)
+        return false;
+    new_node->value = strdup(s);
+    list_add_tail(&new_node->list, head);
     return true;
 }
 
