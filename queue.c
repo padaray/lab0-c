d74b2f4c4dc4cfb59ceb9f9a1a35c21a575e64a7
RayChen <ray870317@gmail.com>
2024-02-28 23:52:30 +0800

    Implement q_size function
    call list_for_each function from list.h, traverse the list
    while calculating its size.

--- queue.c
+++ queue.c
@@ -99,7 +99,14 @@ element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 /* Return number of elements in queue */
 int q_size(struct list_head *head)
 {
-    return -1;
+    int count = 0;
+    struct list_head *node;
+
+    list_for_each (node, head) {
+        count++;
+    }
+
+    return count;
 }
 
 /* Delete the middle node in queue */
