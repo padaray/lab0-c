f6ae25f50b5df3da9fcb8356888a076edca7d072
RayChen <ray870317@gmail.com>
2024-02-28 19:15:09 +0800

    Implement q_new function
    Initialize a space for a list_head and use the INIT_LIST_HEAD function
    from list.h to set head->next and head->prev point to itself.

--- queue.c
+++ queue.c
@@ -14,8 +14,11 @@
 /* Create an empty queue */
 struct list_head *q_new()
 {
-    return NULL;
+    struct list_head *head = malloc(sizeof(struct list_head));
+    INIT_LIST_HEAD(head);
+    return head;
 }
+
 
 /* Free all storage used by queue */
 void q_free(struct list_head *head) {}
