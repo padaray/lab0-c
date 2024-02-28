e83f30154227295ac23f01ac331e47db35bd424e
RayChen <ray870317@gmail.com>
2024-02-28 23:22:44 +0800

    Implement q_remove_tail function
    Use the list_last_entry function from list.h to get the entry,
    then use list_del to break the link, then copy entry->value to
    the parameter sp.

--- queue.c
+++ queue.c
@@ -83,7 +83,17 @@ element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head)
+        return NULL;
+
+    element_t *entry = list_last_entry(head, element_t, list);
+    list_del(&entry->list);
+
+    if (sp) {
+        strncpy(sp, entry->value, bufsize - 1);
+    }
+
+    return entry;
 }
 
 /* Return number of elements in queue */
